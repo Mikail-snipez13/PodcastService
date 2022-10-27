3fb14397346f489696035c833cac468e0e08e29b
Gündüz Mikail <Mikail.Guenduez@swisslife.de>
2022-10-27 20:09:05 +0200

    Improvements: - podcasts can be created/deleted - episodes can be created/deleted

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ __version__ = "0.0.1"
 
 from flask import Flask, request, send_file, jsonify, abort
 from flask_httpauth import HTTPBasicAuth
-from db import Driver, getPodcastPath
+from db import Driver, get_podcast_path
 from werkzeug.security import generate_password_hash, check_password_hash
 import os
 
@@ -12,7 +12,7 @@ auth = HTTPBasicAuth()
 driver = Driver(path="./users.json")
 
 
-# ------------ Auth ------------
+# ------------ AUTH ------------
 @auth.get_user_roles
 def get_user_roles(user):
     return user.roles
@@ -20,7 +20,7 @@ def get_user_roles(user):
 
 @auth.verify_password
 def verify_password(username, password):
-    user = driver.getUser(username)
+    user = driver.get_user(username)
     if user != None and check_password_hash(user.password, password):
         return user
 
@@ -33,34 +33,108 @@ def home():
     # return send_file('./YKWIM.mp3')
 
 
-@app.route('/allUsers', methods=['GET'])
+@app.route('/user/all', methods=['GET'])
 @auth.login_required(role='admin')
 def all_users():
-    return driver.allUsers()
+    return driver.all_users()
 
 
-@app.route('/createUser', methods=['POST'])
+@app.route('/user/create', methods=['POST'])
 @auth.login_required(role='admin')
 def create_user():
-    headers = request.headers
+    body = request.json
     try:
-        if headers["username"] is not None and headers["password"] is not None:
-            if driver.userExists(headers['username']):
+        if body["username"] is not None and body["password"] is not None:
+            if driver.user_exists(body['username']):
                 return {"status": "user exists", "message": "can't create user because the user exists"}
 
-            user = driver.createUser(headers['username'], headers['password'])
+            user = driver.create_user(body['username'], body['password'])
             return {"status": "user created", "user": user.__dict__}
     except KeyError:
-        return {"error": 400, "message": "missing required headers (username, password)"}, 400
+        return {"error": 400, "message": "missing required data (username, password)"}, 400
 
+@app.route('/user/delete', methods=['DELETE'])
+@auth.login_required(role='admin')
+def delete_user():
+    args = request.args
 
-@app.route('/podcast/<name>')
+    try:
+        username = args['username']
+    except KeyError:
+        return {"error": "400", "message": "missing required headers (username)"}, 400
+
+    if driver.delete_user(username) == 1:
+        return {"status": "no user found", "message": "no user to delete"}, 404
+    return {"status": "deleted user", "message": "user {} was deleted".format(username)}
+
+@app.route('/podcast/episode/<name>', methods=['GET'])
 def podcast(name):
-    filenames = next(os.walk('Podcasts'), (None, None, []))[2]
+    filenames = next(os.walk('Podcasts/Audios/'), (None, None, []))[2]
     if name in filenames:
-        return send_file(getPodcastPath(name))
+        return send_file(get_podcast_path(name))
     return jsonify({"error": 404, "message": "no file found"}), 404
 
+@app.route('/podcast/create', methods=['POST'])
+@auth.login_required()
+def create_podcast():
+    body = request.json
+
+    if driver.podcast_exists(auth.current_user().username, body.get('title')):
+        return {"status": "podcast exists",
+                    "message": "The podcast already exists. Please create one with different title."}
+
+    try:
+        driver.create_podcast(auth.current_user().username,
+                              body['title'],
+                              body['link'],
+                              body['description'],
+                              body['language'],
+                              body['author'],
+                              body['image'],
+                              body['explicit'],
+                              body['category'])
+        return {"status": "successful", "message": "created the podcast for {}".format(auth.current_user().username)}
+    except KeyError:
+        return {"error": "400", "message": "missing required data"}, 400
+
+@app.route('/episode/create', methods=['POST'])
+@auth.login_required(role='user')
+def create_episode():
+    data = request.json
+    try:
+        status = driver.add_episode(auth.current_user().username, data)
+        if status == 1:
+            return {"status": "failed", "message": "podcast doesn't exist"}
+        if status == 2:
+            return {"status": "failed", "message": "episode already exists"}
+        return {"status": "successful", "message": "created the episode for {}".format(auth.current_user().username)}
+    except KeyError:
+        return {"error": "400", "message": "missing required data"}, 400
+
+#delete episode
+@app.route('/episode/delete', methods=['DELETE'])
+@auth.login_required(role='user')
+def delete_episode():
+    try:
+        data = request.json
+        if driver.delete_episode(auth.current_user().username, data.get('pod_title'), data.get('ep_title')):
+            return {"status": "successful", "message": "episode was deleted"}
+        return {"status": "failed", "message": "episode doesn't exist"}
+    except KeyError:
+        return {"error": "400", "message": "missing required data (title)"}, 400
+
+
+@app.route('/podcast/delete', methods=['DELETE'])
+@auth.login_required(role='user')
+def delete_podcast():
+    try:
+        data = request.json
+        status = driver.delete_podcast(username=auth.current_user().username, title=data['title'])
+        if status:
+            return {"status": "successful", "message": "podcast was deleted for {}".format(auth.current_user().username)}
+        return {"status": "failed", "message": "no podcast with this title"}
+    except KeyError:
+        return {"error": "400", "message": "missing required data (title)"}
 
 if __name__ == '__main__':
     app.run(host="0.0.0.0", port=5000, threaded=True)
--- db.py
+++ db.py
@@ -1,37 +1,38 @@
 import json
 from werkzeug.security import generate_password_hash
+import datetime
 
 
-def getPodcastPath(name):
-    return 'Podcasts/' + name
+def get_podcast_path(name):
+    return 'Podcasts/Audios/' + name
 
+def get_date_now():
+    return datetime.datetime.now().strftime("%a, %d %h %G %T -01:00")
 
 class Driver:
 
     def __init__(self, path):
         self.path = path
 
-    def getUser(self, username):
-        with open(self.path, 'r') as file:
+    def get_user(self, username):
+        data = self.all_users()
+        users = data['users']
 
-            data = json.load(file)
-            users = data['users']
+        for user in users:
+            if user['username'] == username:
+                return User(user=user)
 
-            for user in users:
-                if user['username'] == username:
-                    return User(user=user)
-
-    def allUsers(self):
+    def all_users(self):
         with open(self.path, 'r') as file:
             data = json.load(file)
             return data
 
-    def userExists(self, username) -> bool:
-        if self.getUser(username) == None:
+    def user_exists(self, username) -> bool:
+        if self.get_user(username) is None:
             return False
         return True
 
-    def createUser(self, username, password):
+    def create_user(self, username, password):
         user = User(username=username, password=password)
         with open(self.path, 'r+') as file:
             data = json.load(file)
@@ -42,6 +43,106 @@ class Driver:
             file.write(json.dumps(data, indent=4))
             file.truncate()
         return user
+
+    def delete_user(self, username):
+        if not self.user_exists(username):
+            return 1
+
+        with open(self.path, "r+") as file:
+            data = json.load(file)
+            data['users'] = [user for user in data.get('users') if user.get('username') != username]
+
+            file.seek(0)
+            file.write(json.dumps(data, indent=4))
+            file.truncate()
+            return 0
+
+    def podcast_exists(self, username, title):
+        podcasts = self.get_user(username).podcasts
+        for podcast in podcasts:
+            if podcast.get('title') == title:
+                return True
+        return False
+
+    def episode_exists(self, username, pod_title, ep_title):
+        podcasts = self.get_user(username).podcasts
+        for podcast in podcasts:
+            if podcast.get('title') == pod_title:
+                items = podcast.get('items')
+                for item in items:
+                    if item.get('title') == ep_title:
+                        return True
+        return False
+
+    def create_podcast(self, username, title, link, description, language, author, image, explicit, category):
+        podcast = Podcast(title, link, description, language, author, image, explicit, category)
+        data = self.all_users()
+        users = data.get('users')
+        for user in users:
+            if user['username'] == username:
+                user['podcasts'].append(podcast.__dict__)
+                with open(self.path, 'w') as file:
+                    file.write(json.dumps(data, indent=4))
+                return
+
+    def delete_podcast(self, username, title):
+        data = self.all_users()
+        users = data.get('users')
+
+        if not self.podcast_exists(username, title):
+            return False
+
+        for user in users:
+            if user['username'] == username:
+                user['podcasts'] = [podcast for podcast in user['podcasts'] if podcast.get('title') != title]
+                with open(self.path, 'w') as file:
+                    file.write(json.dumps(data, indent=4))
+                return True  # if podcast was found
+
+    def add_episode(self, username, data):
+        if not self.podcast_exists(username, data.get('podcast_title')):
+            return 1
+
+        if self.episode_exists(username, data.get('podcast_title'), data.get('title')):
+            return 2
+
+        db = self.all_users()
+        users = db.get('users')
+        for user in users:
+            if user['username'] == username:
+                podcasts = user.get('podcasts')
+                for podcast in podcasts:
+                    if podcast['title'] == data.get('podcast_title'):
+                        items = podcast.get('items')
+                        items.append(Episode(data['title'], data['description'],
+                                             data['author'],
+                                             get_date_now(),
+                                             data['enclosure'],
+                                             data['explicit'],
+                                             data['image'],
+                                             data['keywords']).__dict__)
+                        with open(self.path, 'w') as file:
+                            file.write(json.dumps(db, indent=4))
+                        return 0
+
+    def delete_episode(self, username, pod_title, ep_title):
+        if not self.episode_exists(username, pod_title, ep_title):
+            return False
+
+        data = self.all_users()
+        users = data.get('users')
+        for user in users:
+            if user['username'] == username:
+                podcasts = user.get('podcasts')
+                for podcast in podcasts:
+                    if podcast['title'] == pod_title:
+                        podcast['items'] = [item for item in podcast['items'] if item.get('title') != ep_title]
+                        with open(self.path, 'w') as file:
+                            file.write(json.dumps(data, indent=4))
+                        return True
+        return False
+
+
 
 
 class User:
@@ -58,7 +159,34 @@ class User:
             self.podcasts = user.get('podcasts')
             self.roles = user.get('roles')
 
+    def add_podcast(self, podcast):
+        self.podcasts.append(podcast.__dict__)
 
-
-    def getJSON(self):
+    def get_json(self):
         return json.dumps(self.__dict__, indent=4)
+
+
+class Podcast:
+    def __init__(self, title, link, description, language, author, image, explicit, category):
+        self.title = title
+        self.link = link  # link to rss feed
+        self.description = description
+        self.language = language  # de
+        self.author = author
+        self.image = image
+        self.explicit = explicit  # valid values => no/false/yes/true/clean
+        self.category = category
+        self.type = "episodic"
+        self.items = []
+
+
+class Episode:
+    def __init__(self, title, description, author, pub_date, enclosure, explicit, image, keywords):
+        self.title = title
+        self.description = description
+        self.author = author
+        self.pub_date = pub_date
+        self.enclosure = enclosure
+        self.explicit = explicit
+        self.image = image
+        self.keywords = keywords
